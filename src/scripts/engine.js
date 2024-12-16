29fb10a82afd7f3b43f4788dde6e5e44a1567415
kellenotake <ferreira.kellen@gmail.com>
2024-12-15 21:05:32 -0300

    implementado modificações

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,4 @@
-const emojis =[
+const emojis = [
    "😺",
    "😺",
    "👺",
@@ -16,10 +16,12 @@ const emojis =[
    "🎭",
    "🎭",
 ];
-let openCards = [];
-let shuffleEmojis = emojis.sort(()=> (Math.random()> 0.5 ? 2 : -1));
 
-for(let i=0;i< emojis.length;i++){
+let openCards = [];
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+
+for (let i = 0; i < emojis.length; i++) {
    let box = document.createElement("div");
    box.className = "item";
    box.innerHTML = shuffleEmojis[i];
@@ -27,26 +29,36 @@ for(let i=0;i< emojis.length;i++){
    document.querySelector(".game").appendChild(box);
 }
 
-function handleClick(){
-   if(openCards.length < 2){
+function handleClick() {
+   if (openCards.length < 2) {
       this.classList.add("boxOpen");
       openCards.push(this);
    }
-   if(openCards.length == 2){
-      setTimeout(checkMatch,500);
+   if (openCards.length == 2) {
+      setTimeout(checkMatch, 500);
    }
 }
-function checkMatch(){
-   if(openCards[0].innerHTML === openCards[1].innerHTML){
-     openCards[0].classList.add("boxMatch"); 
-     openCards[1].classList.add("boxMatch"); 
-   }else{
-     openCards[0].classList.remove("boxOpen");
-     openCards[1].classList.remove("boxOpen"); 
+
+function checkMatch() {
+   if (openCards[0].innerHTML === openCards[1].innerHTML) {
+      openCards[0].classList.add("boxMatch");
+      openCards[1].classList.add("boxMatch");
+   } else {
+      openCards[0].classList.remove("boxOpen");
+      openCards[1].classList.remove("boxOpen");
    }
    openCards = [];
 
-   if(document.querySelectorAll(".boxMatch").length === emojis.length){
-      alert("Você venceu!");
+   if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+      showCongratulationMessage()
+
    }
+
+   function showCongratulationMessage() {
+      document.getElementById("congratulation-message").style.display = "block";
+   }
+
+}
+function hideMessage() {
+   document.getElementById("congratulation-message").style.display = 'none';
 }
